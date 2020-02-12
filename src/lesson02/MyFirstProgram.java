f58aa70af3311bac1c4a1fc7f1979f751a8fdf4f
liviv <viktorialvov@gmail.com>
2020-02-12 13:23:45 +0200

    initial commit

--- src/lesson02/MyFirstProgram.java
+++ src/lesson02/MyFirstProgram.java
@@ -3,6 +3,6 @@ package lesson02;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 }
