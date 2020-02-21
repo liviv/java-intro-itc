b74a3db043ae1cec593d897cf7c8d890dd6ac3d6
liviv <viktorialvov@gmail.com>
2020-02-21 19:40:29 +0200

    collections homework  - finished

--- src/lesson05/learn_collections/Program.java
+++ src/lesson05/learn_collections/Program.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 public class Program {
     public static void main(String[] args) {
 
-       ArrayList myArrayList1;
-       myArrayList1 = WorkWithArrayList.createArrayList(5, 2);
+        ArrayList myArrayList1;
+        myArrayList1 = WorkWithArrayList.createArrayList(5, 2);
+        WorkWithArrayList.sortArrayList(myArrayList1);
         WorkWithArrayList.deleteStringFromArray(myArrayList1, 1);
-        WorkWithArrayList.checkIfElementWasDeleted(myArrayList1,"");
     }
 
 
--- src/lesson05/learn_collections/WorkWithArrayList.java
+++ src/lesson05/learn_collections/WorkWithArrayList.java
@@ -1,6 +1,8 @@
 package lesson05.learn_collections;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 
 public class WorkWithArrayList {
     public static ArrayList createArrayList(int count, int length) {
@@ -8,7 +10,7 @@ public class WorkWithArrayList {
         for (int i = 0; i <= count; i++) {
             myArrayList.add(GenerateString.generateString(count, length));
         }
-        System.out.println("My Array List" + myArrayList);
+        System.out.println("My Array List was created" + myArrayList);
         return myArrayList;
     }
 
@@ -16,14 +18,36 @@ public class WorkWithArrayList {
         if (index > myArrayList.size()) {
             throw new IndexOutOfBoundsException("Element with this index does not exist");
         }
-
+        String s = myArrayList.get(index).toString();
         System.out.println(myArrayList.get(index).toString() + " will de deleted");
         myArrayList.remove(index);
+        checkIfElementWasDeleted(myArrayList, s);
+
     }
-    public static boolean checkIfElementWasDeleted(ArrayList myArrayList, String s){
-        boolean result = myArrayList.contains(s)  ;
-        System.out.printf("the string %s was deleted: %s", s, result);
+
+    public static boolean checkIfElementWasDeleted(ArrayList myArrayList, String s) {
+        boolean result = !myArrayList.contains(s);
+        System.out.printf("the string %s was deleted:  %s", s, result);
         return result;
     }
+
+    public static void iterateArrayList(ArrayList myArrayList) {
+        Iterator<String> myIterator = myArrayList.iterator();
+        System.out.println("Iterating array with iterator");
+        while (myIterator.hasNext()) {
+            System.out.println(myIterator.next());
+        }
+    }
+
+    public static void sortArrayList(ArrayList myArrayList) {
+        System.out.println("Printing Array list before sorting" + myArrayList);
+
+        iterateArrayList(myArrayList);
+        Collections.sort(myArrayList);
+        System.out.println("Printing Array list after sorting" + myArrayList);
+        iterateArrayList(myArrayList);
+
+    }
+
 
 }
