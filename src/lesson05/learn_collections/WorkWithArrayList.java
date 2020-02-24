793fc686146e5c9ed5e82d42f16198dab84acfee
Victoria <viktorialvov@gmail.com>
2020-02-24 23:21:18 +0200

    Collections  - improved

--- src/lesson05/learn_collections/WorkWithArrayList.java
+++ src/lesson05/learn_collections/WorkWithArrayList.java
@@ -2,32 +2,37 @@ package lesson05.learn_collections;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 
 public class WorkWithArrayList {
+
+
     public static ArrayList createArrayList(int count, int length) {
         ArrayList myArrayList = new ArrayList<>();
-        for (int i = 0; i <= count; i++) {
-            myArrayList.add(GenerateString.generateString(count, length));
+        for (int i = 0; i < count; i++) {
+            myArrayList.add(GenerateString.generateString(length));
         }
         System.out.println("My Array List was created" + myArrayList);
         return myArrayList;
     }
 
     public static void deleteStringFromArray(ArrayList myArrayList, int index) {
-        if (index > myArrayList.size()) {
+        if (index >= myArrayList.size()) {
             throw new IndexOutOfBoundsException("Element with this index does not exist");
         }
         String s = myArrayList.get(index).toString();
         System.out.println(myArrayList.get(index).toString() + " will de deleted");
         myArrayList.remove(index);
         checkIfElementWasDeleted(myArrayList, s);
+        System.out.println(myArrayList);
 
     }
 
     public static boolean checkIfElementWasDeleted(ArrayList myArrayList, String s) {
         boolean result = !myArrayList.contains(s);
         System.out.printf("the string %s was deleted:  %s", s, result);
+        System.out.println();
         return result;
     }
 
@@ -40,14 +45,16 @@ public class WorkWithArrayList {
     }
 
     public static void sortArrayList(ArrayList myArrayList) {
-        System.out.println("Printing Array list before sorting" + myArrayList);
+        System.out.println("Printing Array list before sorting: " + myArrayList);
 
         iterateArrayList(myArrayList);
-        Collections.sort(myArrayList);
-        System.out.println("Printing Array list after sorting" + myArrayList);
-        iterateArrayList(myArrayList);
+
+        Comparator c2 = (o1, o2) -> {
+            String s1 = (String) o1;
+            String s2 = (String) o2;
+            return s1.toLowerCase().compareTo(s2.toLowerCase());
+        };
+
 
     }
-
-
 }
